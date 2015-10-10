7083e436463b200add4d8298ba12a15fe5a47853
DeemaJS <deeema@outlook.com>
2015-10-10 16:09:24 +0300

    Fourth job done

---
+++ programE.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var filename = process.argv[2];
+
+file = fs.readFile(filename, function(err, data) {
+	contents = data.toString();
+	console.log(contents.split('\n').length - 1);
+})
+
